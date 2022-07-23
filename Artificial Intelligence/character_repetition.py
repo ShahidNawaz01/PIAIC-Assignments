8b92b17bd7a1b3132658e76cbd34c536484a1d63
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-07-23 12:12:41 +0500

    added character repetition assignment

---
+++ Artificial Intelligence/character_repetition.py
@@ -0,0 +1,9 @@
+stat = input('Enter any statement: ')
+char = input('Enter the character of which you want to see repition: ')
+count = 0
+
+for i in stat.lower():
+    if i == char:
+        count += 1
+
+print(f"Character '{char}' is repeated {count} times in above statement.")
