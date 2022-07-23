58c612803683cc7c9a66695a314fde9385370df3
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-07-23 15:09:02 +0500

    modified guess_game assignment

--- Artificial Intelligence/guess_game.py
+++ Artificial Intelligence/guess_game.py
@@ -1,11 +1,10 @@
 import random
 x = random.randint(1, 10)
-guess = int(input('Guess any number b/w 1 to 10: '))
-for i in range(2):
-    if guess != x:
-        guess = input('Guess again: ')
-    else:
+
+for i in range(3):
+    guess = int(input('Guess any number b/w 1 to 10: '))
+    if guess == x:
         print('You won!')
         break
 if guess != x:
-    print('You failed.')
+    print('Try again!')
