2ebefe62267e86dbe737f8b75dfe9e77de1e225d
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-07-30 15:31:50 +0500

    added numbers diamond file

---
+++ Artificial Intelligence/numbers_diamond.py
@@ -0,0 +1,27 @@
+number = int(input('Enter any number b/w 1 to 10: '))
+pattern_def = number
+half_pattern_def = number
+
+for x in range(number):
+    output = ''
+    for y in range(1, 2*number):
+        if y == pattern_def or y == half_pattern_def:
+            output = output + str(pattern_def)
+        else:
+            output = output + ' '
+    print(output)
+    pattern_def -= 1
+    half_pattern_def += 1
+
+pattern_def = 2
+half_pattern_def = 2*number - 2
+for x in range(number - 1):
+    output = ''
+    for y in range(1, 2*number):
+        if y == pattern_def or y == half_pattern_def:
+            output = output + str(pattern_def)
+        else:
+            output = output + ' '
+    print(output)
+    pattern_def += 1
+    half_pattern_def -= 1
