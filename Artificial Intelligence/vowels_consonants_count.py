77a8aaa8c44ccffe0ecf1e874a0e863e26da9c7c
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-07-30 12:34:56 +0500

    added vowels and consonants file

---
+++ Artificial Intelligence/vowels_consonants_count.py
@@ -0,0 +1,12 @@
+stat = input('Please your statement: ')
+vowels_list = ['a', 'e', 'i', 'o', 'u']
+vowels_count = 0
+count = 0
+
+for char in stat.lower():
+    if char in vowels_list:
+            vowels_count += 1
+    if char != '.' and char != ' ':
+        count += 1
+consonants = count - vowels_count
+print(f'In your statement, there are {vowels_count} vowels and {consonants} consonants.')
