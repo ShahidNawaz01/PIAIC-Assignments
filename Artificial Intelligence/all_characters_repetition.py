cfc247f2508c0ed4aae4d1ad29628d64634a0f53
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-08-01 16:18:36 +0500

    modified character repetition count files

--- Artificial Intelligence/all_characters_repetition.py
+++ Artificial Intelligence/all_characters_repetition.py
@@ -1,11 +1,11 @@
 stat = input('Enter your statement: ')
 avoid_repetition = []
 
-for i in stat:
+for i in stat.lower():
     count = 0
     if i not in avoid_repetition:
-        for y in stat:
+        for y in stat.lower():
             if i == y:
                 count += 1
-        print(f'In your statement, {i} is repeated {count} times')
+        print(f"In your statement, '{i}' is repeated {count} times")
     avoid_repetition.append(i)
--- Artificial Intelligence/character_repetition.py
+++ Artificial Intelligence/character_repetition.py
@@ -3,7 +3,7 @@ char = input('Enter the character of which you want to see repition: ')
 count = 0
 
 for i in stat.lower():
-    if i == char:
+    if i == char.lower():
         count += 1
 
 print(f"Character '{char}' is repeated {count} times in above statement.")
