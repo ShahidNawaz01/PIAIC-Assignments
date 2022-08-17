c5dbfaaf357b76c1ab42273c1826f1e83e46e3ba
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-08-17 09:28:09 +0500

    modified student data file

--- Artificial Intelligence/student_data.py
+++ Artificial Intelligence/student_data.py
@@ -11,7 +11,8 @@ for i in range(len(input_parameters)):
 while True:
     courses = int(input('Choose courses you want to enroll in (1 for AIC, 2 for BCC,\
 3 for CNC, 4 for IoT, 5 for Web3.0 and Metaverse Developer): '))
-    selected_courses.append(available_courses[courses-1])
+    if available_courses[courses-1] not in selected_courses:
+        selected_courses.append(available_courses[courses-1])
 
     x = input('Do you want to select any other course? (Y/N) ')
     if x.lower() != 'y':
