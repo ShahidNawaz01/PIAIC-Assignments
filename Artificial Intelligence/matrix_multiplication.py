efa15d88a36d7be4d685d9028a5d2d8020fb88db
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-09-08 22:56:41 +0500

    modified matrix multiplication file

--- Artificial Intelligence/matrix_multiplication.py
+++ Artificial Intelligence/matrix_multiplication.py
@@ -5,7 +5,6 @@ col2 = int(input('Enter the no. of columns of 2nd matrix: '))
 matrix1 = list()
 matrix2 = list()
 prod_matrix = list()
-help_list = list()
 
 if col1 == row2:
     for rows in range(row1):
@@ -22,13 +21,11 @@ if col1 == row2:
         matrix2.append(help_list)
 
     for rows in range(row1):
-        help_list = list()
         for cols in range(col2):
             sum = 0
             for i in range(col1):
                 sum += matrix1[rows][i]*matrix2[i][cols]
-            help_list.append(sum)
-        prod_matrix.append(help_list)
+            prod_matrix.append(sum)
 else:
     print('Columns of matrix 1 should be equal to rows of matrix 2.')
 
